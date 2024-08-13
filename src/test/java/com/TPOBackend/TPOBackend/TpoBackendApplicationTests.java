68ac2f3a10af2aceb53a8472f74cdbebf6d4e114
666tunchi <tinchodotto07@gmail.com>
2024-08-13 20:59:26 -0300

    Primer Push

---
+++ src/test/java/com/TPOBackend/TPOBackend/TpoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TPOBackend.TPOBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
