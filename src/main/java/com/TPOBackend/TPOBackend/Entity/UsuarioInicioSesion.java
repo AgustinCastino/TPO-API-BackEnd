7ff6faf5b5bf7056e54ec8e133b8d64e17c585d6
fanta1805 <santinofloridia1@gmail.com>
2024-08-27 22:05:21 -0300

    Carpeta Entity + UserService
    User service y userControler

---
+++ src/main/java/com/TPOBackend/TPOBackend/Entity/UsuarioInicioSesion.java
@@ -0,0 +1,28 @@
+package com.TPOBackend.TPOBackend.Entity;
+
+public class UsuarioInicioSesion {
+    private String identificador;
+    private String contrasena;
+
+    public UsuarioInicioSesion(String identificador, String contrsena) {
+        this.identificador = identificador;
+        this.contrasena = contrsena;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
+
