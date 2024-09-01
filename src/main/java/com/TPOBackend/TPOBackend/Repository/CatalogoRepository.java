17c29f1dabfb6774f2247d5227266a43dc565097
Agustin-Grande <agusgrandedx@gmail.com>
2024-09-01 18:28:45 -0300

    Entity-Producto

--- src/main/java/com/TPOBackend/TPOBackend/Repository/CatalogoRepository.java
+++ src/main/java/com/TPOBackend/TPOBackend/Repository/CatalogoRepository.java
@@ -1,80 +1,18 @@
 
 package com.TPOBackend.TPOBackend.Repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.TPOBackend.TPOBackend.Repository.Entity.Producto;
 import com.TPOBackend.TPOBackend.Service.ProductoService;
 import java.util.ArrayList;
 import java.util.Optional;
 
 @Repository
-public class CatalogoRepository {
-
-    private final ArrayList<ProductoService> productos = new ArrayList<>();
-
-    public CatalogoRepository(){
-        productos.add(new ProductoService(1, "Coca Cola", "Bebida", "Bebida gaseosa", 10, 100, false, false));
-        productos.add(new ProductoService(2, "Fanta", "Bebida", "Bebida gaseosa", 10, 100, false, false));
-        productos.add(new ProductoService(3, "Sprite", "Bebida", "Bebida gaseosa", 10, 100, false, false));
-        productos.add(new ProductoService(4, "Pepsi", "Bebida", "Bebida gaseosa", 10, 100, false, false));
-        productos.add(new ProductoService(5, "7up", "Bebida", "Bebida gaseosa", 10, 100, false, false));
-        productos.add(new ProductoService(6, "Mirinda", "Bebida", "Bebida gaseosa", 10, 100, false, false));
-        productos.add(new ProductoService(7, "Manaos", "Bebida", "Bebida gaseosa", 10, 100, false, false));
-        productos.add(new ProductoService(8, "Villa del Sur", "Bebida", "Jugo de naranja", 10, 100, false, false));
-        productos.add(new ProductoService(9, "Cepita", "Bebida", "Jugo de naranja", 10, 100, false, false));
-        productos.add(new ProductoService(10, "Naranju", "Bebida", "Jugo de naranja", 10, 100, false, false));
-        productos.add(new ProductoService(11, "Tang", "Bebida", "Jugo en polvo", 10, 100, false, false));
-        productos.add(new ProductoService(12, "Clight", "Bebida", "Jugo en polvo", 10, 100, false, false));
-        productos.add(new ProductoService(13, "Quilmes", "Bebida", "Cerveza", 10, 100, false, false));
-        productos.add(new ProductoService(14, "Brahma", "Bebida", "Cerveza", 10, 100, false, false));
-    }
-
-    public ArrayList<ProductoService> getProductos() {
-        return productos;
-    }
-
-    public void setProductos(ProductoService producto) {
-        this.productos.add(producto);
-    }
-
-    public ProductoService encontrarPorId(int id){
-        return productos.stream().filter(producto -> producto.getId() == id).findFirst().orElse(null);
-    }
-
-    public void eliminarProducto(int id){
-        productos.removeIf(producto -> producto.getId() == id);
-    }
-
-    public void modificarProducto(int id, ProductoService producto){
-        for (int i = 0; i < productos.size(); i++) {
-            if(productos.get(i).getId() == id){
-                productos.set(i, producto);
-            }
-        }
-    }
-
-    public void productoFavorito(int id){
-        for (ProductoService producto : productos) {
-            if(producto.getId() == id){
-                producto.setInFavorites(true);
-            }
-        }
-    }
-
-    public void agregarProducto(ProductoService producto){
-        productos.add(producto);
-    }
-
-    public Optional<ProductoService> encontrarPorCategoria(String categoria){
-        return productos.stream().filter(producto -> producto.getCategory().equals(categoria)).findFirst();
-    }
-
-    public Optional<ProductoService> encontrarPorDestacado(boolean destacado){
-        return productos.stream().filter(producto -> producto.isDestacado() == destacado).findFirst();
-    }
-
-    public Optional<ProductoService> encontrarPorVisto(boolean visto){
-        return productos.stream().filter(producto -> producto.isVisto() == visto).findFirst();
-    }
+public interface CatalogoRepository  extends JpaRepository<Producto, Integer> {
+    Optional<Producto> findById(int id);
+    
 
     
 }
--- src/main/java/com/TPOBackend/TPOBackend/Repository/Entity/Producto.java
+++ src/main/java/com/TPOBackend/TPOBackend/Repository/Entity/Producto.java
@@ -12,4 +12,109 @@ public class Producto {
     @ManyToOne
     @JoinColumn(name = "carrito_id")
     private Carrito carrito;
+
+    private String nombre;
+    private double precio;
+    private int stock;
+    private String liga;
+    private String equipo;
+    private String marca;
+    private String categoria;
+    private String descripcion;
+    private boolean favorito;
+    private boolean visto;
+    private boolean destacaddo;
+
+    public Producto(String nombre,String liga, String equipo,String marca, double precio, int stock, String categoria, String descripcion, boolean destacaddo) {
+        this.nombre = nombre;
+        this.liga = liga;
+        this.equipo = equipo;
+        this.marca = marca;
+        this.precio = precio;
+        this.stock = stock;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+        this.favorito = false;
+        this.visto = false;
+        this.destacaddo = destacaddo;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public Carrito getCarrito() {
+        return carrito;
+    }
+
+    public void setCarrito(Carrito carrito) {
+        this.carrito = carrito;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isFavorito() {
+        return favorito;
+    }
+
+    public void setFavorito(boolean favorito) {
+        this.favorito = favorito;
+    }
+
+    public boolean isVisto() {
+        return visto;
+    }
+
+    public void setVisto(boolean visto) {
+        this.visto = visto;
+    }
+
+    public boolean isDestacaddo() {
+        return destacaddo;
+    }
+
+    public void setDestacaddo(boolean destacaddo) {
+        this.destacaddo = destacaddo;
+    }
+
 }
