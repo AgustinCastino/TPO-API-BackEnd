841b5456422859759e924cb96c99a3038ab136ab
fanta1805 <santinofloridia1@gmail.com>
2024-09-23 23:55:19 -0300

    sacamos import

--- src/main/java/com/TPOBackend/TPOBackend/Controllers/CatalogoProductoController/CatalogoProductoController.java
+++ src/main/java/com/TPOBackend/TPOBackend/Controllers/CatalogoProductoController/CatalogoProductoController.java
@@ -1,20 +1,18 @@
 
 package com.TPOBackend.TPOBackend.Controllers.CatalogoProductoController;
 
-import com.TPOBackend.TPOBackend.Repository.CatalogoRepository;
 import com.TPOBackend.TPOBackend.Service.ProductoService;
 import com.TPOBackend.TPOBackend.Repository.Entity.Producto;
 
-import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/catalogo")
